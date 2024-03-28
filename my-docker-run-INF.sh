d7c395ab20f2ed318f1be21e659c8b3c98f31d91
aldodiaz-UFG <79760784+aldodiaz-UFG@users.noreply.github.com>
2024-03-28 10:41:01 -0300

    Add files via upload

---
+++ my-docker-run-INF.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check if an image name was provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <image-name>"
+    exit 1
+fi
+
+IMAGE_NAME="$1"
+USERNAME=INF0429
+
+# Allow local connections to the X server for GUI applications in Docker
+xhost +local:
+
+# Setup for X11 forwarding to enable GUI
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+# Run the Docker container with the selected image and configurations for GUI applications
+docker run -it --rm \
+    --privileged \
+    --network=host \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --env="ROS_LOCALHOST_ONLY=1" \
+    --env="ROS_DOMAIN_ID=91" \
+    --env="TERM=xterm-256color" \
+    --env LIBGL_ALWAYS_SOFTWARE=0 \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="$HOME/ws/src:/home/$USERNAME/ws/src" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    "$IMAGE_NAME"
