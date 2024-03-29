42e75b971577a6182a7a3744992b45edee03d6a7
aldodiaz-UFG <79760784+aldodiaz-UFG@users.noreply.github.com>
2024-03-29 12:36:05 -0300

    Update my-docker-run-INF.sh

--- my-docker-run-INF.sh
+++ my-docker-run-INF.sh
@@ -19,15 +19,15 @@ xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
 # Run the Docker container with the selected image and configurations for GUI applications
 docker run -it --rm \
+    --name="turtlebot4" \
     --privileged \
     --network=host \
-    --name="turtlebot4" \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
-    --env="ROS_LOCALHOST_ONLY=0" \
+    --env="ROS_LOCALHOST_ONLY=1" \
     --env="ROS_DOMAIN_ID=91" \
     --env="TERM=xterm-256color" \
-    --env LIBGL_ALWAYS_SOFTWARE=1 \
+    --env LIBGL_ALWAYS_SOFTWARE=0 \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --env="XAUTHORITY=$XAUTH" \
     --volume="$XAUTH:$XAUTH" \
